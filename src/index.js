a553709405d7c0265553398be2ca601e91b36e68
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-31 17:14:11 +0200

    init

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { fetchBreeds, fetchCatByBreed, API_KEY } from './js/cat-api';
 
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-fetchBreeds()
-  .then(data => console.log(data))
-  .catch(err => console.log(err.massage));
+fetchBreeds().then(data => console.log(data)).catch(err => console.log(err.massage));
 
 // fetchCatByBreed('abys')
 //   .then(result => console.log(result))
