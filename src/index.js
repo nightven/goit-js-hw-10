9114f3e3150218d7aad6650aadfcf4f0f33c4297
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-31 22:36:36 +0200

    added logik

--- src/index.js
+++ src/index.js
@@ -1,13 +1,67 @@
-import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
-import { fetchBreeds, fetchCatByBreed, API_KEY } from './js/cat-api';
+const refs = {
+  selectEl: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loaderEl: document.querySelector('.loader'),
+  errorEL: document.querySelector('.error')
+};
+const {selectEl, catInfo, loaderEl,errorEL } = refs;
 
-axios.defaults.headers.common['x-api-key'] = API_KEY;
+refs.selectEl.addEventListener('change', onSelect);
 
-fetchBreeds().then(data => console.log(data)).catch(err => console.log(err.massage));
+//chenge select method
+function onSelect(evt) {
+    const breadsId = evt.currentTarget.value;
+    console.log(breadsId);
+    fetchCatByBreed(breadsId);
+}
 
-// fetchCatByBreed('abys')
-//   .then(result => console.log(result))
-//   .catch(err =>
-//     console.log(err.massage)
-//   );
+//get bread method
+fetchBreeds()
+  .then(resp => {
+    console.log(resp.data);
+    setOptionsToSelect(createOptionsMarkup(resp.data));
+  })
+  .catch(err => console.log(err.massage));
+
+//get bread by id method
+fetchCatByBreed(beadId='abys')
+  .then(resp => {
+    console.log(resp.data);
+    showTheCatInfo(createCatInfoMarkup(resp.data));
+  })
+  .catch(err => console.log(err.massage));
+
+
+  //create markup from select bdeads
+function createOptionsMarkup(arr) {
+  return arr
+    .map(({ id, name }) => `<option value='${id}'>${name}</option>`)
+    .join();
+}
+
+//set options to select
+function setOptionsToSelect(markup) {
+  selectEl.innerHTML = markup;
+}
+
+//show once cat
+function showTheCatInfo(markup) {
+  catInfo.innerHTML = markup;
+}
+
+//create markup from once cat
+function createCatInfoMarkup({
+  0: {
+    breeds: {
+      0: { name, description, temperament },
+    },
+    url,
+  },
+}) {
+  return `<img class="cat-img" src="${url}" alt="${name}" width="600">
+    <h2 class="cat-breed">${name}</h2>
+    <p class="cat-descr">${description}</p>
+    <p class="cat-temp"><b>Temperament: </b>${temperament}</p>`;
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,29 +1,21 @@
+import axios from 'axios';
+
 const API_KEY =
   'live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
 function fetchBreeds() {
   // https://api.thecatapi.com/v1/breeds?api_key=live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7
-//   ${BASE_URL}/breeds?api_key=${API_KEY}
-  fetch(`https://api.thecatapi.com/v1/breeds?api_key=live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7`)
-  .then(resp => {
-    console.log(resp)
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
+  //   ${BASE_URL}/breeds?api_key=${API_KEY}
+  return axios.get(`${BASE_URL}/breeds`);
+ 
 }
 
-function fetchCatByBreed(breedId){
-    https://api.thecatapi.com/v1/images/search?breed_ids=abys
-    fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-  .then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
+function fetchCatByBreed(breedId) {
+  //https:api.thecatapi.com/v1/images/search?breed_ids=abys
+  return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
 }
 
-export {fetchBreeds, fetchCatByBreed, API_KEY};
+export { fetchBreeds, fetchCatByBreed };
