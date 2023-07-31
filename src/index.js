1f6e087c7d26276c70626fe52e7938dadb3b9ca4
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-31 17:12:39 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import { fetchBreeds, fetchCatByBreed, API_KEY } from './js/cat-api';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+fetchBreeds()
+  .then(data => console.log(data))
+  .catch(err => console.log(err.massage));
+
+// fetchCatByBreed('abys')
+//   .then(result => console.log(result))
+//   .catch(err =>
+//     console.log(err.massage)
+//   );
---
+++ src/js/cat-api.js
@@ -0,0 +1,29 @@
+const API_KEY =
+  'live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
+  // https://api.thecatapi.com/v1/breeds?api_key=live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7
+//   ${BASE_URL}/breeds?api_key=${API_KEY}
+  fetch(`https://api.thecatapi.com/v1/breeds?api_key=live_dd0iS9bjfaYqfTXyQxs7HmzJnQH9UcuL3VeJ9m57LBzbAUBHcODQS9O5fkUr8YK7`)
+  .then(resp => {
+    console.log(resp)
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+function fetchCatByBreed(breedId){
+    https://api.thecatapi.com/v1/images/search?breed_ids=abys
+    fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+  .then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+export {fetchBreeds, fetchCatByBreed, API_KEY};
